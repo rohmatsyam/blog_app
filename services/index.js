bd58e1d5e51377e16c242b332c6bce0d38b695e7
rohmatsyam <rohmatsyam@yahoo.com>
2022-08-28 16:24:54 +0700

    Post Card

--- services/index.js
+++ services/index.js
@@ -124,6 +124,31 @@ export const getCategories = async () => {
   return result.categories;
 };
 
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetCategoryPost() {
+      posts(where: {featuredPost: true}) {
+        author {
+          name
+          photo {
+            url
+          }
+        }
+        featuredImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }   
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
 export const submitComment = async (obj) => {
   // http request to our own nextjs backend
   const result = await fetch("/api/comments", {
